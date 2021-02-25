30bca0b7d19aafebe6a187a7b51c8be34e5d61aa
shava2Curry <47783146+shava2C@users.noreply.github.com>
2021-02-25 18:28:01 +0900

    完了機能の実装 ・リストから削除する処理の共通化 ・Todoリストからインナーテキストを引っ張ってきて、完了リストに追加

--- src/index.js
+++ src/index.js
@@ -18,16 +18,38 @@ const onClickAdd = () => {
   // buttonタグ
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    // 完了ボタンを押した親要素を取得して、完了TODOに入れる。そのあと削除する
+    deleteFromIncompleatList(completeButton.parentNode);
+
+    //完了リストに追加する
+    const addTarget = completeButton.parentNode;
+
+    // Todo内容Textを取得
+    const text = addTarget.firstElementChild.innerText;
+
+    // div以下を初期化
+    addTarget.textContent = null;
+    const li = document.createElement("li");
+    li.innerText = text;
+    // ボタンタグ
+    // buttonタグ
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻る";
+
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    //完了リストに追加
+    document.getElementById("compleat-list").appendChild(addTarget);
   });
 
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
     //押された削除ボタンの親要素を未完了リストから削除
-    const deleteTarget = deleteButton.parentNode;
-    document.getElementById("incompleat-list").removeChild(deleteTarget);
+    deleteFromIncompleatList(deleteButton.parentNode);
   });
 
   // divタグの子要素に各要素を格納
@@ -36,4 +58,9 @@ const onClickAdd = () => {
   div.appendChild(deleteButton);
 
   document.getElementById("incompleat-list").appendChild(div);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleatList = (target) => {
+  document.getElementById("incompleat-list").removeChild(target);
 };
