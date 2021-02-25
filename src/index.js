4b4687b19f9485efbf98759313a006c0b6bc06eb
shava2Curry <47783146+shava2C@users.noreply.github.com>
2021-02-25 17:39:56 +0900

    タスク追加部分のJS①

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./styles.css";
+
+document.getElementById("addbtn").addEventListener("click", () => onClickAdd());
+
+// テキストボックスを取得し、初期化する
+const onClickAdd = () => {
+  const inputText = document.getElementById("addtxt").value;
+  document.getElementById("addtxt").value = "";
+
+  // div
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // li
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタグの子要素に各要素を格納
+  div.appendChild(li);
+
+  document.getElementById("incompleat-list").appendChild(div);
+};
