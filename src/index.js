5d76ea5062c43d3fe8c62e9524543da7fdda7740
shava2Curry <47783146+shava2C@users.noreply.github.com>
2021-02-25 18:01:14 +0900

    タスクの削除機能実装

--- src/index.js
+++ src/index.js
@@ -25,7 +25,9 @@ const onClickAdd = () => {
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert("削除");
+    //押された削除ボタンの親要素を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incompleat-list").removeChild(deleteTarget);
   });
 
   // divタグの子要素に各要素を格納
